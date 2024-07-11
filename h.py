3a26a6537e821f88f625d6291906d23d59c9d85d
neha <nehavaishnav250@gmail.com>
2024-07-11 12:33:22 +0530

    first commit

---
+++ h.py
@@ -0,0 +1,55 @@
+hungry=True
+if hungry:
+    print("feed me")
+lock='bank'
+if lock=='autoshop':
+ print("cars are cool")
+elif lock=='bank':
+ print("money")
+elif lock=='store':
+  print("I can run")
+else:
+   print("i dont know")
+
+name="poornima"
+if name=="me":
+  print("yes")
+elif name=="op":
+ print("how")
+elif name=="poornima":
+  print("money")
+else :
+ print("hmm")
+
+n=int(input())
+if n%2==0:
+  print("number is prime")
+else:
+  print("number is odd")
+
+l1=[1,2,3,4,5,6,7,8,]
+for num in l1:
+  print("yes")
+for num in  l1:
+ if num%2==0:
+   print("num")
+else:
+ print(type(l1))
+list_sum=0
+for num in l1:
+  list_sum=list_sum+num
+  print(list_sum)
+
+mystring ="Hello World"
+for letter in mystring:
+  print("letter")
+tup=(1,2,3)
+for item in tup:
+  print("item")
+mylist=[(1,2),(1,6),(7,8)]
+len(mylist)
+for(a,b) in mylist:
+  print(a+b)
+d={'k1':1,'k2':2,'k3':3}
+for item in d:
+  print(item)
